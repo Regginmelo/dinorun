9f80cc3a7ed463298f28292553f4abb1d3851114
Regginmelo <meloregin7@gmail.com>
2024-05-21 14:33:01 -0600

    done

--- HUD/HUD.gd
+++ HUD/HUD.gd
@@ -5,7 +5,7 @@ var score = 0
 var highscore = 0
 var is_playing
 func update_score_label():
-	score = score + 100
+	score = score + 10
 	$Scorelabel.text = "score: %d"%(score)
 	
 func update_highscore_label():
